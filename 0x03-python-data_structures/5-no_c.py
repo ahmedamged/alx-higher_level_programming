1dc61d98c7dabc0fe400502d081717a557a2b4d4
AhmedAmged <ahmedamged@live.com>
2024-01-10 09:37:00 +0200

    Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    capitalCounts = my_string.count('C')
+    counts = my_string.count('c')
+    my_string = list(my_string)
+    while(counts):
+        my_string.remove('c')
+        counts -= 1
+    while(capitalCounts):
+        my_string.remove('C')
+        capitalCounts -= 1
+    my_string = ''.join(my_string)
+    return my_string
